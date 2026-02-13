39de122c302b7d356a9394f5a54924a10665c66d
JoshDe777 <73503106+JoshDe777@users.noreply.github.com>
2026-02-13 16:05:32 +0000

    Project Setup

---
+++ code/game.py
@@ -0,0 +1,20 @@
+﻿class Game:
+	def __init__(self, name: str):
+		self.name = name
+		self.running = True
+
+	def draw(self):
+		print("New Frame")
+
+	def onShutdown(self):
+		print("Exiting game.")
+
+	def run(self):
+		while self.running:
+			input_val = input("Press x to exit.\n")
+			if input_val == "x":
+				self.running = False
+
+			self.draw()
+
+		self.onShutdown()
---
+++ code/main.py
@@ -0,0 +1,6 @@
+﻿from game import Game
+
+if __name__ == "__main__":
+	print("Hello World!")
+	game = Game("AI Assignment 1")
+	game.run()
